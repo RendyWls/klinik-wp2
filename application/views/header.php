aebdf44587687cd319fb0bc22ee4a6e350c502e7
rendywls <rendyputra636@gmail.com>
2024-10-30 11:00:36 +0700

    Perubahan navbar menggunakan logo

--- application/views/header.php
+++ application/views/header.php
@@ -12,7 +12,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="<?= base_url() ?>">Klinik</a>
+      <a class="navbar-brand" href="<?= base_url() ?>"><img src="<?= base_url(); ?>assets/images/hospital-transformed.png" alt="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
