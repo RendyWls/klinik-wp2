b927d65edd5ae65d76c73a94b77e0d0df4996ca1
rendywls <rendyputra636@gmail.com>
2024-11-02 22:27:29 +0700

    02/11/2024 - Perubahan Login page awal agar lebih bagus dan rapih

--- application/views/login.php
+++ application/views/login.php
@@ -10,50 +10,74 @@
 </head>
 
 <body class="my-login-page">
-	<section class="h-100">
-		<div class="container h-100">
-			<div class="row justify-content-md-center h-100">
-				<div class="card-wrapper">
-					<div class="brand">
-						<img src="<?= base_url(); ?>assets/images/hospital.png" alt="logo">
-					</div>
-					<div class="card fat">
-						<div class="card-body">
-							<h4 class="card-title">Login</h4>
-							<form method="post" class="my-login-validation" action="<?= base_url('auth/login_aksi') ?>" >
-								<div class="form-group">
-									<label for="email">Username</label>
-									<input id="username" type="text" class="form-control" name="username" value="" required autofocus>
-								</div>
+<section class="vh-100 gradient-custom" style="background-image: url('<?= base_url(); ?>assets/images/background-hospital.jpg'); background-size: cover; background-position: center;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+        <div class="card bg-dark text-white" style="border-radius: 3rem; max-width: 500px; max-height: 600px;">
+          <div class="card-body p-5 text-center">
 
-								<div class="form-group">
-									<label for="password">Password
-										<a href="forgot.html" class="float-right">
-											Forgot Password?
-										</a>
-									</label>
-									<input id="password" type="password" class="form-control" name="password" required data-eye>
-								    <div class="invalid-feedback">
-								    	Password is required
-							    	</div>
-								</div>
+            <div class="mb-md-5 mt-md-4 pb-5">
 
-								<div class="form-group">
-									<div class="custom-checkbox custom-control">
-										<input type="checkbox" name="remember" id="remember" class="custom-control-input">
-										<label for="remember" class="custom-control-label">Remember Me</label>
-									</div>
-								</div>
+			<form method="post" class="mt-4 my-login-validation" action="<?= base_url('auth/login_aksi') ?>">
 
-								<div class="form-group m-0">
-									<button type="submit" class="btn btn-primary btn-block">
-										Login
-									</button>
-								</div>
-								<div class="mt-4 text-center">
-									Don't have an account? <a href="#">Create One</a>
-								</div>
-							</form>
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+			  <br>
+
+              <div data-mdb-input-init class="form-outline form-white mb-4">
+                <input id="username" type="text" name="username"  value="" required autofocus class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">Username</label>
+              </div>
+
+              <div data-mdb-input-init class="form-outline form-white mb-4">
+                <input id="password" type="password" name="password" required data-eye class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+	<!-- <section class="vh-100">
+  	<div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="<?= base_url(); ?>assets/images/hospital.png"
+          class="img-fluid" alt="Sample image">
+      </div>
+	  <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+					<div class=" card fat"> -->
+
+
+			<!-- <<form method="post" class="mt-4 my-login-validation" action="<?= base_url('auth/login_aksi') ?>" > -->
+						<!-- Email input --> 
+			<!-- <div data-mdb-input-init class="form-outline mb-2">
+            	<input id="username" type="text" name="username" class="form-control form-control-lg" value="" required autofocus 
+              		placeholder="Username" />
+            		<label class="form-label" for="form3Example3">Username</label>
+          				</div> -->
+
+<!-- Password input -->
+<!-- <div data-mdb-input-init class="form-outline mb-3">
+            <input id="password" type="password" class="form-control form-control-lg" name="password" required data-eye 
+              placeholder="Enter password" />
+            <label class="form-label" for="form3Example4">Password</label>
+          </div>
+
+								<div class="text-center text-lg-start mt-4 pt-2">
+            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button> -->
+
+
+
+							<!-- </form>
 						</div>
 					</div>
 					<div class="footer">
@@ -62,11 +86,11 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="<?= base_url(); ?>assets/js/login.js"></script>
+	<!-- <script src="<?= base_url(); ?>assets/js/login.js"></script> -->
 </body>
 </html>
